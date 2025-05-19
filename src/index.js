b71bf75d5957efeb8f7adf35f045f0a71f23bb5c
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-05-19 18:45:25 -0500

    Added event listeners to nav buttons
    Successfully added nav button event listeners
    and they work. I need to add a limit to the
    shifting though so that it loops back around
    if you go past the first/last element.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import "./style/style.css";
 import slideShift from "./modules/SlideShift.js";
 
 const slideContainer = document.querySelector(".wide-slide");
+const prevNavButton = document.querySelector(".nav-button.prev");
+const nextNavButton = document.querySelector(".nav-button.next");
 
 const imageSlider = slideShift(slideContainer);
 
 // initial right position set
 imageSlider.resetToFirst();
 
+// add event handlers to nav buttons for prev and next
 
+prevNavButton.addEventListener("click", imageSlider.prevSlide);
 
+nextNavButton.addEventListener("click", imageSlider.nextSlide);
